0e26f6807db4d64732d2727700bbd4070a4b6d6a
Aiden Harvey <aidenharvey102@gmail.com>
2019-06-12 12:05:15 +0000

    added Procfile and gunicorn library for heroku deployment

--- blog/settings.py
+++ blog/settings.py
@@ -88,11 +88,11 @@ if "DATABASE_URL" in os.environ:
 else:
     print("Postgres URL not found. Using SQLite instead")
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
