db7ad571475ee279151ba3fb0f471b4f526dd3ba
NinjaAiden <aidenharvey102@gmail.com>
2019-06-12 13:18:35 +0100

    Delete env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "p0u^egx$agzi002de+xj+(ed4wlpw^#i%_1670$53$0wjy@lo*")
-
-os.environ.setdefault("DATABASE_URL", "postgres://viknephzxpugar:4e0eb9460381eeb4df3c1fad47803dd11fbf96f6b9f2d2efb4ecb9d708bb00ca@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/d25roilku7j2od")
