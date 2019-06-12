45905bcdda96500560239864a7cb6432f8782f01
Aiden Harvey <aidenharvey102@gmail.com>
2019-06-12 12:07:43 +0000

    updated settings file

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),'fsf-blog-app.herokuapp.com']
 
 
 # Application definition
